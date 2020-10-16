3ba683c1b2b9d5e18ee8968ed28e779960e497fa
namonroyr <namonroyr@unal.edu.co>
2020-10-16 09:34:24 -0500

    Quick sort y binary search, se agrega datos de 1 millón de bases.

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,51 @@
 import java.util.*;
 import java.io.*;
+import java.lang.*;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("¿Cuál data set desea elegir (1,2, or 3)? ");
+        //El usuario selecciona el conjunto de datos a comparar.
+        System.out.print("¿Cuál data set desea elegir (1, 2, 3 o 4)? ");
         int testNum = sc.nextInt();
-        if ((testNum < 1) || (testNum > 3)) {
-            System.out.println("Por favor ingrese 1,2, o 3");
+        //Agregar función de Julian.
+        if ((testNum < 1) || (testNum > 4)) {
+            System.out.println("Por favor ingrese 1, 2, 3 o 4");
             return;
         }
+        //Llamado a la función que lee las secuencias.
         String s1 = readSeq("test_data/"+"case" + testNum + "-s1");
         String s2 = readSeq("test_data/"+"case" + testNum + "-s2");
+        //Se imprime el número de bases(caracteres) que tiene cada secuencia.
         if (s1 != null && s2 != null){
             System.out.println("S-1: " + s1.length() + " bases");
             System.out.println("S-2: " + s2.length() + " bases");
         }
-
-        //Se obtiene la longitud de emparejamiento para hacer la comparación
-        //
+        //Se obtiene la longitud de emparejamiento para hacer la comparación.
         System.out.print("Por favor ingrese la longitud de emparejamiento: ");
         int matchLength = sc.nextInt();
+        //Inicia la medición de tiempo.
         long startTime = System.nanoTime();
+        //Se crea Stack de referencia que almacenará los resultados.
         StackRef res;
+        //Verificación no nulidad de cadenas.
         if (s1 == null || s2 == null) {
             throw new AssertionError();
         } else {
+            //Llamado a función principal de comparación
             res = SubStringMatch(s1, s2, matchLength);
         }
+        //Finaliza medición de tiempo.
         long estimatedTime = System.nanoTime() - startTime;
+        //Contador de substrings comunes.
         int countCommon = 0;
         while (!res.isEmpty()){
+            //Pop del Stack de las substrings comunes para imprimirlas una a una.
             System.out.print(res.pop()+" ");
             countCommon++;
         }
+        //
         System.out.println("\nSe encontraron " + countCommon + " subsecuencias comunes.\n");
         System.out.println("Elapsed Time:"+ estimatedTime);
     }
@@ -76,7 +88,44 @@ public class Main {
         return buffer.toString();
     }
 
+    private static int PatternCount(String s, String p){
+        int count = 0;
+        for(int i = 0; i < s.length()-p.length(); i++){
+            String sub = s.substring(i, i+ p.length());
+            if(sub.equals(p))
+                count ++;
+        }
+        return count;
+    }
 
+    private ListArrayGeneric<String> FreqSubstrings(String s, int k){
+        ListArrayGeneric<String> FreqSubs = new ListArrayGeneric();
+        int numOfSubs = s.length() - k + 1;
+        int[] counts = new int[numOfSubs];
+        for (int i = 0; i < s.length()-k; i++){
+            String pattern = s.substring(i, i+k);
+            counts[i] = PatternCount(s, pattern);
+        }
+        int maxCount = maxArr(counts);
+        for (int i = 0; i < s.length()-k; i++){
+            if (counts[i] == maxCount)
+                FreqSubs.insert(s.substring(i, i+k));
+        }
+        return FreqSubs;
+    }
+
+    static int maxArr(int[] arr)
+    {
+        int i;
+        // Initialize maximum element
+        int max = arr[0];
+        // Traverse array elements from second and
+        // compare every element with current max
+        for (i = 1; i < arr.length; i++)
+            if (arr[i] > max)
+                max = arr[i];
+        return max;
+    }
 
     private static StackRef SubStringMatch(String s1, String s2, int matchLength) {
         //ArrayList donde se almacenan las substrings que coinciden
@@ -104,8 +153,8 @@ public class Main {
         System.out.println("\nExisten " + substring_arr2.length + " subcadenas de longitud " + matchLength +" en s2.\n");
 
         //Llamado a función de ordenamiento
-        sortArrayUsingStacks(substring_arr2);
-        //Algoritmo de comparación... por implementar
+        Quicksort(substring_arr2, 0, substring_num2-1);
+        //Algoritmo binario de comparación
         for(int i=0; i < substring_num1; i++){
             int j = binarySearch(substring_arr2,substring_arr1[i]);
             if (j == 1)
@@ -133,45 +182,51 @@ public class Main {
         return c;
     }
 
-    static void sortArrayUsingStacks(String []arr)
+    //QuickSort algorithm
+
+    static int partition(String arr[], int low, int high)
     {
-        // Se hace push de los elementos del array al stack
-        int n = arr.length;
-        StackRef input = new StackRef();
-        for (String s : arr) input.push(s);
-
-        //Se ordena el stack utilizando la función sortStack
-        StackRef sortedStack = sortStack(input);
-
-        // Se asignan los elementos del Stack ordenado al array
-        for (int i = 0; i < n; i++)
+        String pivot = arr[high];
+        int i = (low-1); // index of smaller element
+        for (int j=low; j<high; j++)
         {
-            arr[i] = sortedStack.peek();
-            sortedStack.pop();
+            // If current element is smaller than the pivot
+            if (arr[j].compareTo(pivot) < 0)
+            {
+                i++;
+
+                // swap arr[i] and arr[j]
+                String temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
         }
+
+        // swap arr[i+1] and arr[high] (or pivot)
+        String temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+
+        return i+1;
     }
 
-    static StackRef sortStack(StackRef input)
+    /* The main function that implements QuickSort()
+      arr[] --> Array to be sorted,
+      low  --> Starting index,
+      high  --> Ending index */
+    static void Quicksort(String arr[], int low, int high)
     {
-        StackRef tmpStack = new StackRef();
-
-        while (!input.isEmpty())
+        if (low < high)
         {
-            // Se almacena el elemento top del input stack en una variable temporal
-            // Luego se le hace pop al input
-            String tmp = input.peek();
-            input.pop();
+            /* pi is partitioning index, arr[pi] is
+              now at right place */
+            int pi = partition(arr, low, high);
 
-            // Mientras tmpStack no esté vacío y su top sea menor a temp
-            while (!tmpStack.isEmpty() && tmpStack.peek().compareTo(tmp) < 0)
-            {
-                // se hace pop del tempStack y se empuja(push) al input stack
-                input.push(tmpStack.peek());
-                tmpStack.pop();
-            }
-            tmpStack.push(tmp);
+            // Recursively sort elements before
+            // partition and after partition
+            Quicksort(arr, low, pi-1);
+            Quicksort(arr, pi+1, high);
         }
-        return tmpStack;
     }
 
     //Classes------------------------------------------------------
@@ -221,6 +276,92 @@ public class Main {
         }
         public String peek(){
             return String.valueOf(top.getData());
+        }
+    }
+
+    public class ListArrayGeneric<T extends Comparable<T>> {
+
+        private final int N = 5;
+        private int position, count;
+        private T[] larray;
+        T reference;
+
+        //Constructor
+        public ListArrayGeneric() {
+            count = 0;
+            larray = (T[])new Comparable[N];
+        }
+        //Value returning methods
+        private boolean empty() {
+            return count <= 0;
+        }
+        private boolean full() {
+            return count >= N;
+        }
+        public boolean delete(T item) {
+            boolean deleted = false;
+            if(!empty()) {
+                if(search(item)) {
+                    for(int j = position; j < count-1; j++)
+                        larray[j] = larray[j-1];
+                    count--;
+                    deleted = true;
+                }
+            }
+            else {
+                throw new RuntimeException("List is Empty");
+            }
+            return deleted;
+        }
+        public boolean insert(T item){
+            boolean inserted = false;
+            if(!full()){
+                //Does not insert duplicates
+                if(!search(item)){
+                    for(int j = count; j > position; j--)
+                        larray[j] = larray[j+1];
+                    larray[position] = item;
+                    count++;
+                    inserted = true;
+                }
+            }
+            else
+                throw new RuntimeException("List is Full");
+            return inserted;
+        }
+        private boolean search(T item) {
+            boolean found = false, stop = false;
+            int position = 0;
+            while (position < count && !stop){
+                if(larray[position].compareTo(item) >= 0){
+                    stop = true;
+                    if(larray[position].compareTo(item) == 0)
+                        found = true;
+                }
+                else
+                    position++;
+            }
+            return found;
+        }
+
+        public void output() {
+            System.out.print("List: ");
+            int j = 0;
+            while(j != count) {
+                System.out.print(larray[j]+" ");
+                j++;
+            }
+            System.out.println();
+        }
+        public int CompareTo(T item){
+            int result;
+            if(reference.compareTo(item) > 0)
+                result = 1;
+            else if (reference.compareTo(item) < 0)
+                result = -1;
+            else
+                result = 0;
+            return result;
         }
     }
 
