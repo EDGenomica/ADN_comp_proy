5b7f466374a800983005f39b033cd875e70a87f5
Victor <vmtorresa>
2020-11-14 14:11:31 -0500

    La clase MenuMainGUI ya tiene agrupados los botones.  Está en proceso de programar los eventos del botón aceptar.

--- src/MainMenuGUI.java
+++ src/MainMenuGUI.java
@@ -7,6 +7,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JTextPane;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -41,47 +42,79 @@ public class MainMenuGUI extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblMenuPrincBienvenidos = new JLabel("\u00A1BIENVENIDOS A ADA SOLUTIONS!");
 		lblMenuPrincBienvenidos.setBounds(203, 11, 169, 14);
 		contentPane.add(lblMenuPrincBienvenidos);
-		
+
 		JLabel lblMenuPrincTitulo = new JLabel("SISTEMA DE GESTION Y ANALISIS DE ADN");
 		lblMenuPrincTitulo.setBounds(183, 36, 204, 14);
 		contentPane.add(lblMenuPrincTitulo);
-		
-		JLabel lblMenuPrincSeleccione = new JLabel("Seleccione una opci\u00F3n entre 1 y 4 o digite la letra Q para salir.");
+
+		JLabel lblMenuPrincSeleccione = new JLabel(
+				"Seleccione una opci\u00F3n entre 1 y 4 o digite la letra Q para salir.");
 		lblMenuPrincSeleccione.setBounds(55, 73, 297, 14);
 		contentPane.add(lblMenuPrincSeleccione);
-		
+
+		ButtonGroup menuPrincGrupo = new ButtonGroup(); // Clase ButtonGroup obliga a que sólo una opción sea seleccionada.
+
 		JRadioButton rdbtnMenuPrincOpc1 = new JRadioButton("1 Compara secuencias de ADN - porcentaje de similitud.");
 		rdbtnMenuPrincOpc1.setBounds(41, 105, 395, 23);
 		contentPane.add(rdbtnMenuPrincOpc1);
-		
+
 		JRadioButton rdbtnMenuPrincOpc2 = new JRadioButton("2 Subcadenas m\u00E1s frecuentes en una secuencia.");
 		rdbtnMenuPrincOpc2.setBounds(41, 131, 395, 23);
 		contentPane.add(rdbtnMenuPrincOpc2);
-		
+
 		JRadioButton rbtnMenuPrincOpc3 = new JRadioButton("3 Ocurrencia de una subcacena en una secuencia.");
 		rbtnMenuPrincOpc3.setBounds(41, 157, 395, 23);
 		contentPane.add(rbtnMenuPrincOpc3);
-		
+
 		JRadioButton rbtnMenuPrincOpc4 = new JRadioButton("4 Complemento reverso de una secuencia.");
 		rbtnMenuPrincOpc4.setBounds(41, 183, 395, 23);
 		contentPane.add(rbtnMenuPrincOpc4);
-		
+
 		JRadioButton rbtnMenuPrincOpcSalir = new JRadioButton("Salir.");
 		rbtnMenuPrincOpcSalir.setBounds(41, 209, 395, 23);
 		contentPane.add(rbtnMenuPrincOpcSalir);
-		
+
+		menuPrincGrupo.add(rdbtnMenuPrincOpc1);
+		menuPrincGrupo.add(rdbtnMenuPrincOpc2);
+		menuPrincGrupo.add(rbtnMenuPrincOpc3);
+		menuPrincGrupo.add(rbtnMenuPrincOpc4);
+		menuPrincGrupo.add(rbtnMenuPrincOpcSalir);
+
 		JButton btnMenuPrincAcpetar = new JButton("Aceptar");
 		btnMenuPrincAcpetar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+			public void actionPerformed(ActionEvent arg0) { // Según la opción escogida, acá se programa el evento del
+															// botón aceptar.
+				boolean q = true;
+				while (q) {
+					if (arg0.getSource() == contentPane.add(rdbtnMenuPrincOpc1)) {
+						Main.compMenu();
+						Main.mainMenu();
+					} else if (arg0.getSource() == contentPane.add(rdbtnMenuPrincOpc2)) {
+						Main.subFreqMenu();
+						Main.mainMenu();
+//					} else if (arg0.getSource() == rbtnMenuPrincOpc3) {
+//						Main.occSubMenu();
+//						Main.mainMenu();
+//					} else if (arg0.getSource() == rbtnMenuPrincOpc4) {
+//						Main.revSeqMenu();
+//						Main.mainMenu();
+//					} else if (arg0.getSource() == rbtnMenuPrincOpcSalir) {
+//						q = false;
+//						System.exit(0);
+//					} else {
+//						System.out.println("Ingrese una opciÃ³n vÃ¡lida");
+//						Main.mainMenuPrint();
+					}
+				}
 			}
 		});
 		btnMenuPrincAcpetar.setBounds(83, 269, 89, 23);
 		contentPane.add(btnMenuPrincAcpetar);
-		
+
 		JButton btnMenuPrincSalir = new JButton("Salir");
 		btnMenuPrincSalir.setBounds(401, 269, 89, 23);
 		contentPane.add(btnMenuPrincSalir);
